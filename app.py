ab85ce50651a32ddcc537761a68a5b9587949e87
Kurt <kurt.dietrich.85@gmail.com>
2021-04-22 19:26:25 -0400

    Incomplete HW-10 completed 4/22/2021 - final commit

--- app.py
+++ app.py
@@ -1,24 +1,184 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, and_
+
 from flask import Flask, jsonify
 
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
 
 app = Flask(__name__)
 
-Unfortunately, I ran out of time to complete the second part of the homework this week. I did invest 20 hours on it.
 
 
 @app.route("/")
-def home():
-    return "Hi"
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;"
+    )
 
 
-@app.route("/normal")
-def normal():
-    return hello_dict
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+     # Find the most recent date in the data set.
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()  
+    # Calculate the date one year from the last date in data set.
+    year_ago = dt.date(2017, 8 ,23) - dt.timedelta(days=365)
+    year_ago = year_ago.strftime('%Y-%m-%d')
+
+    dates = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>year_ago).order_by(Measurement.date.desc()).all()
+    # df = pd.DataFrame(dates, columns =['Date', 'Prcp'])
+    # df = df[df.Date > year_ago]
+   
+
+    all_results = []
+    for date, prcp in dates:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        all_results.append(precipitation_dict)
+
+    session.close()
+
+    return jsonify(all_results)
 
 
-@app.route("/jsonified")
-def jsonified():
-    return jsonify(hello_dict)
+@app.route("/api/v1.0/stations")
+def stations():
+    stations = {}
+
+    # Query all stations
+    results = session.query(Station.station, Station.name).all()
+    for s,name in results:
+        stations[s] = name
+
+    session.close()
+ 
+    return jsonify(stations)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    # Find the most recent date in the data set.
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()  
+    # Calculate the date one year from the last date in data set.
+    year_ago = dt.date(2017, 8 ,23) - dt.timedelta(days=365)
+    year_ago = year_ago.strftime('%Y-%m-%d')
+    
+    # Query for the dates and temperature values
+    # Use "year_ago" from precipitation route
+    results =   session.query(Measurement.date, Measurement.tobs).\
+                filter(Measurement.date >= year_ago).\
+                order_by(Measurement.date).all()
+
+    # Convert to list of dictionaries to jsonify
+    tobs_dates = []
+
+    for date, tobs in results:
+        new_dict = {}
+        new_dict[date] = tobs
+        tobs_dates.append(new_dict)
+
+    session.close()
+
+    return jsonify(tobs_dates)
+
+
+@app.route("/api/v1.0/<start>")
+def temp_range_start(start):
+    """TMIN, TAVG, and TMAX per date starting from a starting date.
+    
+    Args:
+        start (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+
+    return_list = []
+
+    results =   session.query(  Measurement.date,\
+                                func.min(Measurement.tobs), \
+                                func.avg(Measurement.tobs), \
+                                func.max(Measurement.tobs)).\
+                        filter(Measurement.date >= start).\
+                        group_by(Measurement.date).all()
+
+    for date, min, avg, max in results:
+        new_dict = {}
+        new_dict["Date"] = date
+        new_dict["TMIN"] = min
+        new_dict["TAVG"] = avg
+        new_dict["TMAX"] = max
+        return_list.append(new_dict)
+
+    session.close()    
+
+    return jsonify(return_list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temp_range_start_end(start,end):
+    """TMIN, TAVG, and TMAX per date for a date range.
+    
+    Args:
+        start (string): A date string in the format %Y-%m-%d
+        end (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+
+    # Create our session (link) from Python
+
+    return_list = []
+
+    results =   session.query(  Measurement.date,\
+                                func.min(Measurement.tobs), \
+                                func.avg(Measurement.tobs), \
+                                func.max(Measurement.tobs)).\
+                        filter(and_(Measurement.date >= start, Measurement.date <= end)).\
+                        group_by(Measurement.date).all()
+
+    for date, min, avg, max in results:
+        new_dict = {}
+        new_dict["Date"] = date
+        new_dict["TMIN"] = min
+        new_dict["TAVG"] = avg
+        new_dict["TMAX"] = max
+        return_list.append(new_dict)
+
+    session.close()    
+
+    return jsonify(return_list)
 
 
 if __name__ == "__main__":
